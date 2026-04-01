99df9db36b3a6686aba227b816a79bb7960f81a4
minhduc08122007-maker <minhduc08122007@gmail.com>
2026-04-01 15:00:08 +0700

    Add initial README with project title

---
+++ vuive/src/kosao.java
@@ -0,0 +1,9 @@
+public class kosao {
+    void main(){
+        int tong = 0;
+        for (int i = 1; i <= 10; i++) {
+            tong += i;
+        }
+        System.out.println(tong);
+    }
+}
